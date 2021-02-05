68dda12e5543eabbea04981b6159b78f2ab0ce14
jun991203 <jun991203@naver.com>
2021-02-06 03:57:46 +0900

    add: intro, login, register, tos dialog 화면 생성

---
+++ app/src/main/java/com/dot/jyp/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.dot.jyp.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dot.jyp.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
