9b90fb3c18a35607b1b2537790782ffe93651ed1
jun991203 <jun991203@naver.com>
2021-02-18 21:15:11 +0900

    update: activity_login.xml, activity_register.xml 수정

--- app/src/main/java/com/dot/jyp/login/StartActivity.kt
+++ app/src/main/java/com/dot/jyp/login/StartActivity.kt
@@ -1,12 +1,31 @@
 package com.dot.jyp.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dot.jyp.R
+import com.dot.jyp.databinding.ActivityLoginBinding
+import com.dot.jyp.databinding.ActivityStartBinding
+import com.dot.jyp.lobby.LobbyActivity
 
 class StartActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityStartBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStartMemberLogin.setOnClickListener {
+            val loginIntent = Intent(this, LoginActivity::class.java)
+            startActivity(loginIntent)
+        }
+
+        binding.btnStartNonmemLogin.setOnClickListener {
+            val lobbyIntent = Intent(this, LobbyActivity::class.java)
+            startActivity(lobbyIntent)
+        }
     }
 }
