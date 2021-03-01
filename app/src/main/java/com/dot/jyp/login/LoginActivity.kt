9cdb91e8c3c88e4c317677c9a6aa15c398ff0d9e
jun991203 <jun991203@naver.com>
2021-03-01 18:24:26 +0900

    fix: 유효성검사, 버튼 활성화(로그인)

--- app/src/main/java/com/dot/jyp/login/LoginActivity.kt
+++ app/src/main/java/com/dot/jyp/login/LoginActivity.kt
@@ -1,8 +1,13 @@
 package com.dot.jyp.login
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.core.content.ContextCompat
+import com.dot.jyp.R
 import com.dot.jyp.lobby.LobbyActivity
 import com.dot.jyp.databinding.ActivityLoginBinding
 import com.dot.jyp.register.RegisterActivity
@@ -16,16 +21,66 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //--로그인 기능------------------------------------------------------------------------------
+        var isEmailChecked = false
+        binding.edittextLoginEmail.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
+            override fun afterTextChanged(p0: Editable?) {
+                if(binding.edittextLoginEmail.text.toString() != ""){
+                    if(binding.edittextLoginPwd.text.toString() != ""){
+                        binding.btnLogin.setBackgroundResource(R.drawable.btn_blue_200)
+                        binding.btnLogin.setTextColor(ContextCompat.getColor(applicationContext, R.color.colorWhite))
+                        binding.btnLogin.isEnabled = true
+                        binding.btnLogin.isClickable = true
+                    }
+                    isEmailChecked = true
+                }else{
+                    binding.btnLogin.setBackgroundResource(R.drawable.btn_black_200)
+                    binding.btnLogin.setTextColor(ContextCompat.getColor(applicationContext, R.color.colorGrey2))
+                    binding.btnLogin.isEnabled = false
+                    binding.btnLogin.isClickable = false
+                    isEmailChecked = false
+                }
+            }
+        })
+
+        binding.edittextLoginPwd.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if(isEmailChecked && binding.edittextLoginPwd.text.toString() != ""){
+                    binding.btnLogin.setBackgroundResource(R.drawable.btn_blue_200)
+                    binding.btnLogin.setTextColor(ContextCompat.getColor(applicationContext, R.color.colorWhite))
+                    binding.btnLogin.isEnabled = true
+                    binding.btnLogin.isClickable = true
+                }else{
+                    binding.btnLogin.setBackgroundResource(R.drawable.btn_black_200)
+                    binding.btnLogin.setTextColor(ContextCompat.getColor(applicationContext, R.color.colorGrey2))
+                    binding.btnLogin.isEnabled = false
+                    binding.btnLogin.isClickable = false
+                }
+            }
+            override fun afterTextChanged(p0: Editable?) { }
+        })
+
         // 임시 로그인 : login -> lobby
         binding.btnLogin.setOnClickListener {
             val lobbyIntent = Intent(this, LobbyActivity::class.java)
             startActivity(lobbyIntent)
         }
 
+
         //--옵션 기능--------------------------------------------------------------------------------
         // 자동 로그인
+        var autoCheck :Boolean = false
         binding.llLoginAuto.setOnClickListener {
-
+            if(autoCheck){
+                autoCheck = false
+                binding.imageLoginAuto.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.colorMainBlue))
+            }else{
+                autoCheck = true
+                binding.imageLoginAuto.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.colorGrey2))
+            }
         }
 
 
--- app/src/main/java/com/dot/jyp/register/RegisterActivity.kt
+++ app/src/main/java/com/dot/jyp/register/RegisterActivity.kt
@@ -8,6 +8,7 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import com.dot.jyp.R
@@ -28,23 +29,32 @@ class RegisterActivity : AppCompatActivity() {
 
         //--이메일-----------------------------------------------------------------------------------
         // 이메일 유효성 검사
-       binding.edittextRegisterEmail.addTextChangedListener(object: TextWatcher{
+        var isEmailChecked = false
+        binding.edittextRegisterEmail.addTextChangedListener(object: TextWatcher{
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
             override fun afterTextChanged(p0: Editable?) {
                 if(!android.util.Patterns.EMAIL_ADDRESS.matcher(p0.toString()).matches()){
                     binding.textRegisterEmailWrong.text = "이메일 형식을 확인해주세요."
                     binding.edittextRegisterEmail.setBackgroundResource(R.drawable.edit_error)
+                    binding.btnRegisterEmailCheck.setBackgroundResource(R.drawable.btn_black_200)
+                    binding.btnRegisterEmailCheck.setTextColor(ContextCompat.getColor(applicationContext, R.color.colorGrey2))
+                    binding.btnRegisterEmailCheck.isClickable = false
+                    binding.btnRegisterEmailCheck.isEnabled = false
                 }else{
                     binding.textRegisterEmailWrong.text = ""
                     binding.edittextRegisterEmail.setBackgroundResource(R.drawable.edit_activate)
+                    binding.btnRegisterEmailCheck.setBackgroundResource(R.drawable.btn_blue_200)
+                    binding.btnRegisterEmailCheck.setTextColor(ContextCompat.getColor(applicationContext, R.color.colorWhite))
+                    binding.btnRegisterEmailCheck.isClickable = true
+                    binding.btnRegisterEmailCheck.isEnabled = true
                 }
             }
         })
 
         // 이메일 중복 체크 버튼 액션
         binding.btnRegisterEmailCheck.setOnClickListener {
-
+            Toast.makeText(applicationContext, "이메일 중복 확인", Toast.LENGTH_SHORT).show()
         }
 
         //--비밀번호---------------------------------------------------------------------------------
@@ -64,7 +74,6 @@ class RegisterActivity : AppCompatActivity() {
                     binding.textRegisterPwdWrong.setTextColor(ContextCompat.getColor(applicationContext, R.color.colorError))
                 }
             }
-
         })
 
         // 비밀번호 확인 검사
@@ -74,13 +83,23 @@ class RegisterActivity : AppCompatActivity() {
                 if (!binding.edittextRegisterPwd.text.toString().equals(binding.edittextRegisterPwd2.text.toString())){
                     binding.edittextRegisterPwd.setBackgroundResource(R.drawable.edit_error)
                     binding.edittextRegisterPwd2.setBackgroundResource(R.drawable.edit_error)
+                    binding.textRegisterPwdWrong.setTextColor(ContextCompat.getColor(applicationContext, R.color.colorError))
                     binding.textRegisterPwdWrong.text = "비밀번호를 확인해주세요."
-                    binding.textRegisterEmailWrong.setTextColor(ContextCompat.getColor(applicationContext, R.color.colorError))
+                    // 회원가입 버튼 설정
+                    binding.btnRegister.setBackgroundResource(R.drawable.btn_black_200)
+                    binding.btnRegister.setTextColor(ContextCompat.getColor(applicationContext, R.color.colorGrey2))
+                    binding.btnRegister.isClickable = false
+                    binding.btnRegister.isEnabled = false
                 }else{
                     binding.edittextRegisterPwd.setBackgroundResource(R.drawable.edit_check)
                     binding.edittextRegisterPwd2.setBackgroundResource(R.drawable.edit_check)
-                    binding.textRegisterPwdWrong.text = "사용 가능한 비밀번호입니다."
                     binding.textRegisterPwdWrong.setTextColor(ContextCompat.getColor(applicationContext, R.color.colorCheck))
+                    binding.textRegisterPwdWrong.text = "사용 가능한 비밀번호입니다."
+                    // 회원가입 버튼 설정
+                    binding.btnRegister.setBackgroundResource(R.drawable.btn_blue_200)
+                    binding.btnRegister.setTextColor(ContextCompat.getColor(applicationContext, R.color.colorWhite))
+                    binding.btnRegister.isClickable = true
+                    binding.btnRegister.isEnabled = true
                 }
             }
             override fun afterTextChanged(p0: Editable?) {
@@ -92,7 +111,6 @@ class RegisterActivity : AppCompatActivity() {
             val loginIntent = Intent(this, LoginActivity::class.java)
             startActivity(loginIntent)
         }
-
     }
 
 
